51cec99011401b90adc6637b7846825ca69a29fd
Mats Kruskopf Eriksson <knishopf@gmail.com>
2026-05-26 15:33:10 +0200

    remove frontend

--- backend/start-with-crac.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Start application without CRaC.
-java -jar /app/app.jar
-
-# todo implement CRaC
-# https://docs.spring.io/spring-framework/reference/integration/checkpoint-restore.html
-# https://codefarm0.medium.com/coordinated-restore-at-checkpoint-crac-in-spring-boot-b39f1cb9e76f
